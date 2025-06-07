a9cabdb185e374328a5ec4fe3647a8866ae03b0f
KhushiAgrawal0407 <khushiagrawal0407@gmail.com>
2025-06-07 16:28:27 +0530

    committing code

---
+++ PrimeBasket/src/main/java/com/ecommerce/PrimeBasket/PrimeBasketApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.PrimeBasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeBasketApplication.class, args);
+	}
+
+}
