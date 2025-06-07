03f06d5865867e3da5bdf4cb16b7224d3333f1c3
KhushiAgrawal0407 <khushiagrawal0407@gmail.com>
2025-06-07 16:28:27 +0530

    committing code

---
+++ PrimeBasket/src/test/java/com/ecommerce/PrimeBasket/PrimeBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.PrimeBasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
